e365a9918ac4df88a8f3db8824ca82fa7333c204
Roberto Gambuzzi <gambuzzi@gbinside.com>
2013-06-19 17:27:01 +0200

    test1 now take a ini file as mandatory first parameter

--- demo/test1.py
+++ demo/test1.py
@@ -3,11 +3,18 @@ import sys
 
 sys.path.append('..')
 import pygento
+import ConfigParser
 
 __author__ = 'roberto'
 
 if __name__ == "__main__":
-    conn = pygento.Magento('http://terminalvideo.local/index.php/api/xmlrpc/', 'python', 'password')
+    config = ConfigParser.ConfigParser()
+    config.read(sys.argv[1])
+    conn = pygento.Magento(
+        config.get('General', 'url', ''),
+        config.get('General', 'user', ''),
+        config.get('General', 'pass', '')
+    )
     pprint(conn.treeCategory(1))
     prodotto = conn.getProductInfo('573600')
     del prodotto['price']
